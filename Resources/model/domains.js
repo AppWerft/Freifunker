d40966ad892a7e6cd5c10d41c977af1049064778
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2015-07-06 16:29:52 +0100

    new domains

---
+++ Resources/model/domains.js
@@ -0,0 +1,100 @@
+module.exports = [{
+	"name" : "Aachen",
+	"url" : "http://map.freifunk-aachen.de/nodes.json"
+}, {
+	"name" : "Bielefeld",
+	"url" : "http://map.freifunk-bielefeld.de/nodes.json"
+}, {
+	"name" : "Darmstadt",
+	"url" : "http://map.darmstadt.freifunk.net/nodes.json"
+}, {
+	"name" : "Dortmund",
+	"url" : "http://map.ffdo.de/data/nodes.json"
+}, {
+	"name" : "Dresden",
+	"url" : "http://info.freifunk-dresden.de/info/nodes.json"
+}, {
+	"name" : "Freiburg",
+	"url" : "http://freiburg.freifunk.net/ffmap-d3/nodes.json"
+}, {
+	"name" : "Fulda",
+	"url" : "http://map.freifunk-fulda.de/nodes.json"
+}, {
+	"name" : "Göttingen",
+	"url" : "https://cccgoe.de/map/nodes.json"
+}, {
+	"name" : "Gütersloh",
+	"url" : "http://stats.guetersloh.freifunk.net/map/nodes.json"
+}, {
+	"name" : "Hamburg",
+	"url" : "https://graph.hamburg.freifunk.net/nodes.json"
+}, {
+	"name" : "Hannover",
+	"url" : "http://hannover.freifunk.net/karte/nodes.json"
+}, {
+	"name" : "Hildesheim",
+	"url" : "http://map.freifunk-hi.de/json/nodes.json"
+}, {
+	"name" : "Ingolstadt",
+	"url" : "https://freifunk-ingolstadt.de/map/nodes.json"
+}, {
+	"name" : "Jena",
+	"url" : "https://freifunk-jena.de/ffmap/nodes.json"
+}, {
+	"name" : "Karlsruhe",
+	"url" : "http://www.freifunk-karlsruhe.de/map/nodes.json"
+}, {
+	"name" : "Kiel",
+	"url" : "http://map.freifunk.in-kiel.de/json/nodes.json"
+}, {
+	"name" : "Köln/Bonn",
+	"url" : "http://map.kbu.freifunk.net/nodes.json"
+}, {
+	"name" : "Lüneburg",
+	"url" : "http://freifunk-lueneburg.de/karte/nodes.json"
+}, {
+	"name" : "Magdeburg",
+	"url" : "http://map.md.freifunk.net/nodes.json"
+}, {
+	"name" : "Mainz",
+	"url" : "http://map.freifunk-mainz.de/nodes.json"
+}, {
+	"name" : "Möhne",
+	"url" : "http://map.freifunk-moehne.de/data/nodes.json"
+}, {
+	"name" : "München",
+	"url" : "http://map.freifunk-muenchen.de/nodes.json"
+}, {
+	"name" : "Münster",
+	"url" : "https://freifunk-muenster.de/map/nodes.json"
+}, {
+	"name" : "Ostholstein",
+	"url" : "http://ostholstein.freifunk.net/map/nodes.json"
+}, {
+	"name" : "Paderborn",
+	"url" : "http://map.paderborn.freifunk.net/nodes.json"
+}, {
+	"name" : "Radevormwald",
+	"url" : "http://map.gm.wupper.freifunk-rheinland.net/nodes.json"
+}, {
+	"name" : "Rhein Neckar",
+	"url" : "http://map.freifunk-rhein-neckar.de/nodes.json"
+}, {
+	"name" : "Rheinland",
+	"url" : "http://ffmap.freifunk-rheinland.net/nodes.json"
+}, {
+	"name" : "Ruhrgebiet",
+	"url" : "http://map.freifunk-ruhrgebiet.de/nodes.json"
+}, {
+	"name" : "Troisdorf",
+	"url" : "http://map.freifunk-troisdorf.de/nodes.json"
+}, {
+	"name" : "Uelzen",
+	"url" : "http://map.freifunk-uelzen.de/nodes.json"
+}, {
+	"name" : "Westpfalz",
+	"url" : "http://map.freifunk-westpfalz.de/nodes.json"
+}, {
+	"name" : "Wuppertal",
+	"url" : "http://map.freifunk-wuppertal.net/nodes.json"
+}]; 
