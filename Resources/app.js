cdaa39723a7e39eecde3f722c7b32f2bc1340506
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2015-09-08 10:22:03 +0200

    Refugees

--- Resources/app.js
+++ Resources/app.js
@@ -1,8 +1,7 @@
 ;! function() {
-	/*var GeoTools = require('de.appwerft.geotools');
-	GeoTools.loadKML('http://asylantenheime.square7.ch/files/uebersicht_zu_asylantenheimen_in_deutschland.kml').then(function(_e) {
+	require('de.appwerft.geotools').loadKML(Ti.App.Properties.getString('refugees')).then(function(_e) {
 		console.log(_e);
-	});*/
+	});
 	require('ui/main')();
 	new (require('adapter/domainlist'))();
 	require('adapter/feed')({});
